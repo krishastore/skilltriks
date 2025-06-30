e3b3817dda12ba1b8eab5dc5ece7bec428cae2e5
Dhruvang2001 <dhruvang@krishaweb.com>
2025-06-30 14:48:23 +0530

    Fix:251 Remove duplicate name from dropdown

--- templates/frontend/assign-course-by-me.php
+++ templates/frontend/assign-course-by-me.php
@@ -20,6 +20,8 @@ foreach ( $course_assigned_by_me as $key => $completion_date ) :
 	list( $course_id, $_user_id ) = explode( '_', $key, 2 );
 	$stlms_users[]                = get_userdata( $_user_id )->display_name;
 endforeach;
+
+$stlms_users = array_unique( $stlms_users );
 ?>
 <div class="stlms-wrap alignfull">
 	<?php require_once STLMS_TEMPLATEPATH . '/frontend/sub-header.php'; ?>
